b19929cf51d93f46b91aaab6aa8935ce7d50da75
Libx09 <ibrahimfuad729@gmail.com>
2024-12-23 00:21:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+//middle were
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iciu9bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const artifactsCollection = client
+      .db("TimekeepersArchiveDB")
+      .collection("artifacts");
+
+    app.get("/artifacts/limit", async (req, res) => {
+      const limit = 6;
+      const cursor = artifactsCollection.find().limit(limit);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Timekeeper's Archive Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Timekeeper's Archive Server is Running on Port: ${port}`);
+});
